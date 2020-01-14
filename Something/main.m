6922ea1a7607128d4c94e6a89245277318a7e219
haleonas <altes@live.se>
2020-01-14 19:07:20 +0100

    Initial Commit

---
+++ Something/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Something
+//
+//  Created by Jesper Stenlund on 2020-01-14.
+//  Copyright © 2020 Jesper Stenlund. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
